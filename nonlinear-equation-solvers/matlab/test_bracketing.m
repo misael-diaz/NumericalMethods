4993284b815a7eff3b34237b81e9a9537696ffdc
misael-diaz <misael.diaz@upr.edu>
2021-07-28 10:42:56 -0400

    selects interval in which hybrid converges faster

--- nonlinear-equation-solvers/matlab/test_bracketing.m
+++ nonlinear-equation-solvers/matlab/test_bracketing.m
@@ -27,7 +27,7 @@ clc
 format long g
 
 
-a = 2.0e-2;	b = 7.0e-2;	% bracketing interval [a, b]
+a = 1.0e-2;	b = 9.0e-2;	% bracketing interval [a, b]
 
 
 % defines the nonlinear function f(x) as a lambda function
@@ -38,3 +38,4 @@ f = @(x) 1.0 / sqrt(x) + 2.0 * log10(0.024651/3.7 + ...
 % solves for the root of f(x) numerically
 x = bisect(a, b, f)	% Bisection
 x = regfal(a, b, f)	% Regula Falsi (or False Position)
+x = shifter(a, b, f)	% Hybrid
