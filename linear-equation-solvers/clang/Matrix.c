5031152cafb0d85a30c653a43fffcd4e08f3e6e1
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2022-09-05 17:34:29 -0400

    partitions memory management utilities [clang]
    COMMENTS:
    Moves code that often gets duplicated into suitable methods. In the
    end the code becomes easier to read and follow.
    
    These methods shall be used in future commits. I thought the timing
    to add them was just right.
    
    The code produces the expected output.
    
    Valgrind reports no memory leaks.

--- linear-equation-solvers/clang/Matrix.c
+++ linear-equation-solvers/clang/Matrix.c
@@ -43,6 +43,52 @@ static double get_method (void *matrix, size_t i, size_t j)
 }
 
 
+static matrix_t* util_alloc_matrix_t ()
+// allocates memory for a matrix object
+{
+	matrix_t *mat = (matrix_t*) malloc ( sizeof(matrix_t) );
+	if (mat == NULL)
+	{
+		char errmsg [] = "memory allocation error: %s\n";
+		fprintf (stderr, errmsg, strerror(errno) );
+                exit(EXIT_FAILURE);
+        }
+
+	return mat;
+}
+
+
+static double* util_alloc_array_double_t (size_t size)
+// allocates memory for an array of doubles of requested size
+{
+	double *array = (double*) malloc ( size * sizeof(double) );
+	if (array == NULL)
+	{
+		char errmsg [] = "memory allocation error: %s\n";
+		fprintf (stderr, errmsg, strerror(errno) );
+                exit(EXIT_FAILURE);
+        }
+
+	return array;
+}
+
+
+static double* util_init_from_array_double_t (
+	size_t r, size_t c, size_t sz, double array[r][c], double data[sz]
+)
+// initializes the first-rank array `data' from the second-rank array
+{
+	size_t rows = r, cols = c;
+	for (size_t i = 0; i != rows; ++i)
+	{
+		for (size_t j = 0; j != cols; ++j)
+			data[j + i * cols] = array[i][j];
+	}
+
+	return data;
+}
+
+
 static matrix_t* destroy (matrix_t* mat)
 // destroys the matrix by freeing the allocated resources
 {
@@ -62,35 +108,20 @@ static matrix_t* create (size_t rows, size_t cols, double A[rows][cols])
 // constructs a matrix from a second-rank array
 {
 	// allocates memory for the matrix
-	matrix_t *mat = (matrix_t*) malloc ( sizeof(matrix_t) );
-	if (mat == NULL)
-	{
-		char errmsg [] = "memory allocation error: %s\n";
-		fprintf (stderr, errmsg, strerror(errno) );
-                exit(EXIT_FAILURE);
-        }
+	matrix_t *mat = util_alloc_matrix_t ();
 
+	size_t size = (rows * cols);
 	// allocates memory for the data buffer of the matrix
-	mat -> buffer = (double*) malloc ( rows * cols * sizeof(double) );
-	if (mat -> buffer == NULL)
-	{
-		char errmsg [] = "memory allocation error: %s\n";
-		fprintf (stderr, errmsg, strerror(errno) );
-                exit(EXIT_FAILURE);
-        }
+	mat -> buffer = util_alloc_array_double_t (size);
 
 	double *data = mat -> buffer;
 	// copies the second-rank array into the data buffer of the matrix
-	for (size_t i = 0; i != rows; ++i)
-	{
-		for (size_t j = 0; j != cols; ++j)
-			data[j + i * cols] = A[i][j];
-	}
+	data = util_init_from_array_double_t (rows, cols, size, A, data);
 
 	// defines the matrix shape and size
 	mat -> rows = rows;
 	mat -> cols = cols;
-	mat -> size = (rows * cols);
+	mat -> size = size;
 	// binds methods
 	mat -> get  = get_method;
 	return mat;
